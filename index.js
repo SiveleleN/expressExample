a866a0fdd3d921c85ff61b6915c7bc4fc6f9e60f
SiveleleN <sivelelenkamane29@gmail.com>
2024-02-09 16:16:12 +0200

    added

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Welcome to Express Js")
+// console.log("Welcome to Express Js")
+import express from 'express'
+// const express = require('express')
+
+//create an express app
+const app = express()
+const port = +process.env.PORT || 4000 
+//Router
+
+app.get('/',(req,res)=>{
+    //    res.status(200).json({
+    //     msg: 'You\'re home'
+    //    })
+    res.json({
+        status: res.statusCode,
+        msg: 'You\'re home'
+    })
+})
+app.get('/about', (req, res)=>{clear
+    res.json({
+        status: res.statusCode,
+        msg: 'About Page'
+    })
+})
+app.all('*', (req, res)=>{
+    res.json({
+        status: 404,
+        msg: '404 page'
+    })
+})
+
+app.listen(port)
