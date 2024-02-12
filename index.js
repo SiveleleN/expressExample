dd90c89984fc887b683801a849ae3ed893730085
SiveleleN <sivelelenkamane29@gmail.com>
2024-02-12 14:09:00 +0200

    file crearted

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // console.log("Welcome to Express Js")
+// 1st step is to import the server
+import { json } from 'body-parser'
 import express from 'express'
+import path from 'path'
 // const express = require('express')
-
 //create an express app
 const app = express()
-const port = +process.env.PORT || 4000 
+const router = express.Router()
+const port = +process.env.PORT || 4000
+app.use(router,
+    express.static('./static')
+)
 //Router
-
-app.get('/',(req,res)=>{
+router.get('/$|/express',display ,(req,res)=>{
+    res.json
     //    res.status(200).json({
     //     msg: 'You\'re home'
     //    })
@@ -16,17 +22,17 @@ app.get('/',(req,res)=>{
         msg: 'You\'re home'
     })
 })
-app.get('/about', (req, res)=>{clear
+router.get('/about', (req, res)=>{clear
     res.json({
         status: res.statusCode,
         msg: 'About Page'
     })
 })
-app.all('*', (req, res)=>{
-    res.json({
-        status: 404,
-        msg: '404 page'
-    })
-})
-
+//middlaware
+function display(req, res, next){
+     console.log("Hello there");
+     next()
+}
+// For running the port server
+// last step
 app.listen(port)
